8a7e86f26eb1afdc7c458124a21c5587ce1337bd
Jason Watkins <goushoryuken@gmail.com>
2022-06-28 07:11:42 -0700

    simple format

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_202832) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_185012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "airports", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "symbol"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "flight_id"
+    t.integer "passenger_id"
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
+  end
 
   create_table "dmeets", force: :cascade do |t|
     t.text "dmeet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "origin_id"
+    t.integer "destination_id"
+    t.string "number"
+    t.datetime "departure"
+    t.index ["destination_id"], name: "index_flights_on_destination_id"
+    t.index ["origin_id"], name: "index_flights_on_origin_id"
+  end
+
+  create_table "kittens", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.integer "cuteness"
+    t.integer "softness"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +79,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_202832) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "bookings", "passengers"
+  add_foreign_key "flights", "airports", column: "destination_id"
+  add_foreign_key "flights", "airports", column: "origin_id"
 end
